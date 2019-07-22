51cc8ac056ae5add5d440837f3122c255c688098
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-22 04:27:17 +0200

    fix typo in Scream.js

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -7,7 +7,6 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typorgraphy from '@material-ui/core/Typography';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -48,14 +47,14 @@ class Scream extends Component {
                 className={classes.image}
             />
             <CardContent className={classes.content}>
-                <Typorgraphy
+                <Typography
                 variant="h5"
                 component={Link}
                 to={`/users/${userHandle}`}
                 color="primary"
                 >
                 {userHandle}
-                </Typorgraphy>
+                </Typography>
                 <Typography variant="body2" color="textSecondary">
                 {dayjs(createdAt).fromNow()}
                 </Typography>
