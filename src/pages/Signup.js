023804b432dac96a417b4cfb5e74822c43096883
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-25 21:58:57 +0200

    fix styles with MUI theme

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.ico';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import theme from '../util/theme';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -11,53 +12,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-// const styles = (theme) => ({
-//     ...theme
-// });
-
-const styles = {
-    palette: {
-        primary: {
-        light: '#33c9dc',
-        main: '#00bcd4',
-        dark: '#008394',
-        contrastText: '#fff'
-        },
-        secondary: {
-        light: '#ff6333',
-        main: '#ff3d00',
-        dark: '#b22a00',
-        contrastText: '#fff'
-        }
-    },
-    typography: {
-        useNextVariants: true
-    },
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10
-    },
-        progress: {
-        position: 'absolute'
-    }
-};
+const styles = theme;
 
 class signup extends Component {
     constructor() {
