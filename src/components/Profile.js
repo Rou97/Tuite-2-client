5ae62ff6e49841a6c647bbc4a861271d0c42cf7b
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-29 12:54:08 +0200

    add logout adn edit ptofile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,8 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 
+import EditDetails from './EditDetails';
+
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
@@ -15,6 +17,7 @@ import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
 import EditIcon from '@material-ui/icons/Edit';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 
 import { connect } from 'react-redux';
 import { logoutUser, uploadImage } from '../redux/actions/userActions';
@@ -78,6 +81,9 @@ class Profile extends Component {
         const fileInput = document.getElementById('imageInput');
         fileInput.click();
     };
+    handleLogout = () => {
+        this.props.logoutUser();
+    };
     render() {
         const {
             classes,
@@ -138,6 +144,12 @@ class Profile extends Component {
                             <CalendarToday color="primary" />{' '}
                             <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                         </div>
+                        <Tooltip title="Logout" placement="top">
+                            <IconButton onClick={this.handleLogout}>
+                                <KeyboardReturn color="primary" />
+                            </IconButton>
+                        </Tooltip>
+                        <EditDetails />
                     </div>
                 </Paper>
             ) : (
--- src/util/theme.js
+++ src/util/theme.js
@@ -1,16 +1,16 @@
 export default {
     palette: {
         primary: {
-        light: '#33c9dc',
-        main: '#00bcd4',
-        dark: '#008394',
-        contrastText: '#fff'
+            light: '#33c9dc',
+            main: '#00bcd4',
+            dark: '#008394',
+            contrastText: '#fff'
         },
         secondary: {
-        light: '#ff6333',
-        main: '#ff3d00',
-        dark: '#b22a00',
-        contrastText: '#fff'
+            light: '#ff6333',
+            main: '#ff3d00',
+            dark: '#b22a00',
+            contrastText: '#fff'
         }
     },
     typography: {
@@ -37,7 +37,10 @@ export default {
         fontSize: '0.8rem',
         marginTop: 10
     },
-        progress: {
+    progress: {
         position: 'absolute'
+    },
+    editButton: {
+        float: 'right'
     }
 };
