7769057e71a475a47625e79dbeda8c43e9f278fb
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-29 21:20:24 +0200

    add like and unlike

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
 
-import Scream from '../components/Scream'
+import Scream from '../components/Scream';
 import Profile from '../components/Profile';
 
-class Home extends Component {
-    state = {
-        screams: null
-    };
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
 
+class home extends Component {
     componentDidMount() {
-        axios
-            .get('/screams')
-            .then((res) => {
-                this.setState({
-                    screams: res.data
-                });
-            })
-            .catch((err) => console.log(err));
+        this.props.getScreams();
     }
-
     render() {
-        let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
+        const { screams, loading } = this.props.data;
+        let recentScreamsMarkup = !loading ? (
+            screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
         ) : (
                 <p>Loading...</p>
             );
         return (
-            <Grid container spacing={10}>
+            <Grid container spacing={16}>
                 <Grid item sm={8} xs={12}>
                     {recentScreamsMarkup}
                 </Grid>
@@ -36,8 +28,20 @@ class Home extends Component {
                     <Profile />
                 </Grid>
             </Grid>
-        )
+        );
     }
 }
 
-export default Home
+home.propTypes = {
+    getScreams: PropTypes.func.isRequired,
+    data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    data: state.data
+});
+
+export default connect(
+    mapStateToProps,
+    { getScreams }
+)(home);
