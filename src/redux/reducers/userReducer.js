f2a314b2cb75d58c4ad9e86b1d4bd6a017b6005f
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-29 08:50:57 +0200

    add material-ui/icons and profile component

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,25 +1,37 @@
-import { SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED } from '../types';
-  
+import {
+    SET_USER,
+    SET_AUTHENTICATED,
+    SET_UNAUTHENTICATED,
+    LOADING_USER
+} from '../types';
+
 const initialState = {
     authenticated: false,
+    loading: false,
     credentials: {},
     likes: [],
     notifications: []
 };
-  
-export default function(state = initialState, action) {
+
+export default function (state = initialState, action) {
     switch (action.type) {
         case SET_AUTHENTICATED:
             return {
                 ...state,
-                authenticated: true 
+                authenticated: true
             };
         case SET_UNAUTHENTICATED:
             return initialState;
         case SET_USER:
             return {
                 authenticated: true,
+                loading: false,
                 ...action.payload
+            };
+        case LOADING_USER:
+            return {
+                ...state,
+                loading: true
             };
         default:
             return state;
