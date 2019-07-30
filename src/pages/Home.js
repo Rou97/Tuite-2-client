33da9bddaf7c113ca6c5f4cf98b5b5c582563ef9
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-30 09:31:02 +0200

    delete button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ class home extends Component {
                 <p>Loading...</p>
             );
         return (
-            <Grid container spacing={16}>
+            <Grid container spacing={10}>
                 <Grid item sm={8} xs={12}>
                     {recentScreamsMarkup}
                 </Grid>
