ae83086339d47f741f7d44428d5a36f8f9933bee
Raul Delgado <raul.delgado.diaz97@gmail.com>
2019-07-31 08:04:29 +0200

    added comments on screams

--- src/components/scream/ScreamDialog.js
+++ src/components/scream/ScreamDialog.js
@@ -4,6 +4,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import MyButton from '../../util/MyButton';
 import LikeButton from './LikeButton';
 import Comments from './Comments';
+import CommentForm from './CommentForm';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
 import theme from '../../util/theme';
@@ -19,7 +20,7 @@ import UnfoldMore from '@material-ui/icons/UnfoldMore';
 import ChatIcon from '@material-ui/icons/Chat';
 
 import { connect } from 'react-redux';
-import { getScream } from '../../redux/actions/dataActions';
+import { getScream, clearErrors } from '../../redux/actions/dataActions';
 
 const styles = {
     ...theme,
@@ -57,6 +58,7 @@ class ScreamDialog extends Component {
     };
     handleClose = () => {
         this.setState({ open: false });
+        this.props.clearErrors();
     };
 
     render() {
@@ -106,8 +108,8 @@ class ScreamDialog extends Component {
                         </MyButton>
                         <span>{commentCount} comments</span>
                     </Grid>
-                    {/* TODO: comment input */}
                     <hr className={classes.visibleSeparator} />
+                    <CommentForm screamId={screamId} />
                     <Comments comments={comments} />
                 </Grid>
             );
@@ -143,6 +145,7 @@ class ScreamDialog extends Component {
 }
 
 ScreamDialog.propTypes = {
+    clearErrors: PropTypes.func.isRequired,
     getScream: PropTypes.func.isRequired,
     screamId: PropTypes.string.isRequired,
     userHandle: PropTypes.string.isRequired,
@@ -156,7 +159,8 @@ const mapStateToProps = (state) => ({
 });
 
 const mapActionsToProps = {
-    getScream
+    getScream,
+    clearErrors
 };
 
 export default connect(
